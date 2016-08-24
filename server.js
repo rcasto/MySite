284d7843d1758a4e4bf114790e42b26858bd39d4
Richard Casto <rcasto92@gmail.com>
2016-08-24 01:38:22 -0700

    remove cors middleware, small comment updates

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var Poet = require('poet');
 
 var port = process.env.PORT || 3000;
 var environment = process.env.NODE_ENV || 'development';
-var app = express();
-
 var poet = Poet(app, {
    posts: './posts/',
    postsPerPage: 5,
    metaFormat: 'json'
 });
+var app = express();
 
+// Setup PoetJS for Blogging
 poet.addRoute('/blog/:post', function (req, res) {
     var post = poet.helpers.getPost(req.params.post);
     if (post) {
@@ -25,7 +25,7 @@ poet.addRoute('/blog/:post', function (req, res) {
     console.error(err);
 });
 
-// set views directory, used for rendering blog posts
+// Set views directory and view engine, used for rendering blog posts
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'pug');
 
@@ -34,13 +34,6 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-// Middleware to apply CORS headers for GitHub
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 // Homepage route
 app.get('/', function (req, res) {
     res.render('index', {
@@ -48,7 +41,7 @@ app.get('/', function (req, res) {
     });
 });
 
-// blog api endpoint
+// Blog API endpoint
 app.get('/api/blog/:post', function (req, res) {
     var post = poet.helpers.getPost(req.params.post);
     if (post) {
